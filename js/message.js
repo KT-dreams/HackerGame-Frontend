e7090d8ec0d0694a76a182688287c055b949fd15
MKathy <kathrine1418@gmail.com>
2020-06-21 13:10:45 +0200

    ESLint, .gitignore added.

--- js/message.js
+++ js/message.js
@@ -1,36 +1,39 @@
-function send() {
+function send() { // eslint-disable-line no-unused-vars
 
-var data = document.getElementById("data");
+   var data = document.getElementById("data");
 
-data.onkeypress = function(event) {
-	if (event.keyCode == 13 || event.which == 13){
-		
-		var dat = data.value;
-		var result = dat.split(" ");
-		var command = result[0];
-		var value = result[1];
-		
-	    var http = new XMLHttpRequest();
-	    var url = 'http://localhost:8000/' + command;
-	    var params = 'command=' + command + '&value=' + value;
-	    http.open("POST", url, true);
+   data.onkeypress = function(event) {
+      if (event.keyCode == 13 || event.which == 13) {
+         
+         var x = data.value;
+         var result = x.split(" ");
+         var command = result[0];
+         var value = result[1];
+         
+         var http = new XMLHttpRequest();
+         var url = "http://localhost:8000/" + command;
+         var params = JSON.stringify({
+           command: command,
+           value: value
+         });
+         http.open("POST", url, true);
+         http.setRequestHeader("Content-Type", "application/json");
 
-	    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-	   	http.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				myFunction('Anonymous $ ' + dat);
-				myFunction(this.responseText);
-			}
-		};
-		http.send(params);
-		data.value = '';
-	};
+         http.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+               add_paragraph("Anonymous $ " + x);
+               add_paragraph(this.responseText);
+            }
+         };
+         http.send(params);
+         data.value = "";
+      }
+   };
 }
-}
 
-function myFunction(text) {
-	  var para = document.createElement("P");
-	  var t = document.createTextNode(text);
-	  para.appendChild(t);
-	  document.getElementById("commands").appendChild(para);
+function add_paragraph(text) {
+     var p = document.createElement("P");
+     var t = document.createTextNode(text);
+     p.appendChild(t);
+     document.getElementById("commands").appendChild(p);
 }
